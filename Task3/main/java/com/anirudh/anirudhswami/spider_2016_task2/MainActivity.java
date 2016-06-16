59f8ba13f620e4f972b52852d1fd3311fea86eee
Anirudh <aniswami97@gmail.com>
2016-06-17 01:27:20 +0530

    Song starts and stop working

--- Task3/main/java/com/anirudh/anirudhswami/spider_2016_task2/MainActivity.java
+++ Task3/main/java/com/anirudh/anirudhswami/spider_2016_task2/MainActivity.java
@@ -5,15 +5,22 @@ import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     private static int[] images = {R.mipmap.green_circle,R.mipmap.orange_triangle,R.mipmap.blue_pentagon,R.mipmap.red_square,R.mipmap.yellow_hexagon};
     ImageView img;
     int i = 0;
+    String song;
+    Button play,stop;
+
+    PlaySong p;// = new PlaySong(MainActivity.this,play,stop);
 
     Handler handler = new Handler(){
         @Override
@@ -31,6 +38,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        stop = (Button) findViewById(R.id.stop);
+        stop.setEnabled(false);
+        play = (Button) findViewById(R.id.play);
+
+        Spinner spinner = (Spinner) findViewById(R.id.songs);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(MainActivity.this,R.array.Songs,R.layout.support_simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
 
         img = (ImageView) findViewById(R.id.slideShow);
 
@@ -57,5 +74,50 @@ public class MainActivity extends AppCompatActivity {
                 t.start();
             }
         });
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Playing song " + song, Toast.LENGTH_SHORT).show();
+                p  = new PlaySong(MainActivity.this,play,stop);
+                p.execute(song);
+                play.setEnabled(false);
+                stop.setEnabled(true);
+                Toast.makeText(MainActivity.this, "Cancelled is " + p.isCancelled(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"The song isCancelled value was "+p.isCancelled(),Toast.LENGTH_SHORT).show();
+                p.cancel(true);
+                p.mp.stop();
+                p.mp.release();
+                play.setEnabled(true);
+                stop.setEnabled(false);
+                Toast.makeText(MainActivity.this,"The song isCancelled value is "+p.isCancelled(),Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        song = parent.getItemAtPosition(position).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        Toast.makeText(MainActivity.this,"Nothing is selected",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(MainActivity.this,"The song isCancelled value was "+p.isCancelled(), Toast.LENGTH_SHORT).show();
+        p.cancel(true);
+        p.mp.stop();
+        p.mp.release();
+        Toast.makeText(MainActivity.this,"The song isCancelled value is "+p.isCancelled(),Toast.LENGTH_SHORT).show();
     }
 }
---
+++ Task3/main/java/com/anirudh/anirudhswami/spider_2016_task2/PlaySong.java
@@ -0,0 +1,84 @@
+package com.anirudh.anirudhswami.spider_2016_task2;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+/**
+ * Created by Anirudh Swami on 17-06-2016.
+ */
+public class PlaySong extends AsyncTask<String,Void,String> implements MediaPlayer.OnCompletionListener{
+
+    MediaPlayer mp;
+    Context context;
+    Button button1;
+    Button button2;
+
+    public PlaySong(Context ctx,Button btn1,Button btn2){
+        this.context = ctx;
+        this.button1 = btn1;
+        this.button2 = btn2;
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        String song = params[0];
+        switch (song){
+            case "Awake":
+                mp = MediaPlayer.create(context,R.raw.awake);
+                break;
+            case "Boulevard":
+                mp = MediaPlayer.create(context,R.raw.boulevard);
+                break;
+            case "Hero":
+                mp = MediaPlayer.create(context,R.raw.hero);
+                break;
+            case "Numb":
+                mp = MediaPlayer.create(context,R.raw.numb);
+                break;
+            case "Roads":
+                mp = MediaPlayer.create(context,R.raw.roads);
+                break;
+            default:
+                Toast.makeText(context,"The song is not right",Toast.LENGTH_SHORT).show();
+        }
+        mp.start();
+        mp.setOnCompletionListener(this);
+        long progress = mp.getCurrentPosition();
+        while(progress<mp.getDuration() && mp.isPlaying() && !isCancelled()){
+            progress = mp.getCurrentPosition();
+            if(isCancelled()){
+                progress = mp.getDuration();
+                Toast.makeText(context,"Task is being  cancelled",Toast.LENGTH_SHORT).show();
+                break;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+        Toast.makeText(context,"Started song successfully",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        Toast.makeText(context,"Song finished ",Toast.LENGTH_SHORT).show();
+        button1.setEnabled(true);
+        button2.setEnabled(false);
+    }
+
+    @Override
+    protected void onCancelled() {
+        super.onCancelled();
+        Toast.makeText(context,"Task was cancelled",Toast.LENGTH_SHORT).show();
+        button1.setEnabled(true);
+        button2.setEnabled(false);
+    }
+}
